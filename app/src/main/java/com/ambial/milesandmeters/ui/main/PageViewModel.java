bc5422c83f533a5349f4086c1456edc64a9e5482
Simon Körner <simonkoerner@gmx.de>
2020-01-12 21:25:33 +0100

    WIP init MVVM

---
+++ app/src/main/java/com/ambial/milesandmeters/ui/main/PageViewModel.java
@@ -0,0 +1,20 @@
+package com.ambial.milesandmeters.ui.main;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class PageViewModel extends ViewModel {
+    /**
+     * Live Data Instance
+     */
+    private MutableLiveData<String> mName = new MutableLiveData<>();
+
+    public void setName(String name) {
+        mName.setValue(name);
+    }
+
+    public LiveData<String> getName() {
+        return mName;
+    }
+}
